71b9980d91a527ffa04726aac24a8235c7b51fe3
Rodrick Sims <rsimstransport@gmail.com>
2021-09-06 13:55:21 -0400

    updated routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
+const server = require('./server');
+//const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
 
